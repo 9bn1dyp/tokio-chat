b67069efb946ea7e9f3e5d70e63fe4950877acd6
9bn1dyp <9bn1dyp@gmail.com>
2025-12-06 03:33:45 +0000

    feat: basic tcp chat server using broadcast
    - Setup TcpListener
    - Implemented client read/write loops using tokio and broadcast
    - Minor error handling
    
    note: msg display bug. todo!, better error handling; better ui, usernames.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,33 +1,59 @@
+use anyhow::Result;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
+use tokio::sync::broadcast::{self, Receiver, Sender};
+
+async fn client_read_loop(
+    mut rx: Receiver<String>,
+    mut socket_write: OwnedWriteHalf,
+) -> anyhow::Result<()> {
+    loop {
+        let mut bytes = rx.recv().await?;
+        bytes.push('\n');
+
+        if let Err(e) = socket_write.write_all(bytes.as_bytes()).await {
+            eprintln!("Failed reading chat {e}")
+        }
+    }
+}
+
+async fn client_write_loop(
+    tx: Sender<String>,
+    mut socket_read: OwnedReadHalf,
+) -> anyhow::Result<()> {
+    loop {
+        let mut buf = [0u8; 1024];
+        let n = socket_read.read(&mut buf).await?;
+
+        let msg = String::from_utf8_lossy(&buf[..n]).to_string();
+
+        if let Err(e) = tx.send(msg) {
+            eprintln!("Failed sending message {e}")
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
-
+    // broadcast
+    let (tx, _) = broadcast::channel::<String>(16);
     loop {
-        let (mut socket, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
+        // accept and split
+        let (socket, _) = listener.accept().await?;
+        let split = socket.into_split();
 
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(0) => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
+        // clone sender
+        let tx_clone = tx.clone();
+        // create receiver from tx
+        let rx = tx.subscribe();
 
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
-            }
-        });
+        // read is reading chat (broadcast output), requires write socket
+        // needs a receiver
+        tokio::spawn(client_read_loop(rx, split.1));
+
+        // write is writing to receivers (broadcast input), requires read socket
+        tokio::spawn(client_write_loop(tx_clone, split.0));
     }
 }
