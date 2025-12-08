c930eadf5aade406a789147c4086115625783cc4
9bn1dyp <9bn1dyp@gmail.com>
2025-12-08 23:22:34 +0000

    refactor: move tcp server to frame-based parsing
    - replace raw byte stream parsing with frames, default lines codec
    - add new Message type

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,48 +1,83 @@
 use anyhow::Result;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use futures::{SinkExt, StreamExt};
+use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 use tokio::sync::broadcast::{self, Receiver, Sender};
+use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
+
+// Req for serde
+#[derive(Deserialize, Serialize)]
+struct User(String);
+
+// To pass into broadcast we need the Clone trait
+impl Clone for User {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+// Req for serde
+#[derive(Deserialize, Serialize)]
+struct Message {
+    username: User,
+    message: String,
+}
+
+// To pass into broadcast we need the Clone trait
+impl Clone for Message {
+    fn clone(&self) -> Self {
+        Self {
+            username: self.username.clone(),
+            message: self.message.clone(),
+        }
+    }
+}
 
 async fn client_read_loop(
-    mut rx: Receiver<String>,
-    mut socket_write: OwnedWriteHalf,
+    mut rx: Receiver<Message>,
+    mut writer: FramedWrite<OwnedWriteHalf, LinesCodec>,
 ) -> anyhow::Result<()> {
     loop {
-        let mut bytes = rx.recv().await?;
-        bytes.push_str("\r\n");
+        // get Message type from broadcast
+        let message_struct = rx.recv().await?;
 
-        if let Err(e) = socket_write.write_all(bytes.as_bytes()).await {
+        // convert to json
+        let message_struct = serde_json::to_string(&message_struct)?;
+
+        if let Err(e) = writer.send(message_struct).await {
             eprintln!("Failed reading chat {e}")
         }
     }
 }
 
 async fn client_write_loop(
-    tx: Sender<String>,
-    mut socket_read: OwnedReadHalf,
+    tx: Sender<Message>,
+    mut reader: FramedRead<OwnedReadHalf, LinesCodec>,
 ) -> anyhow::Result<()> {
-    loop {
-        let mut buf = [0u8; 1024];
-        let n = socket_read.read(&mut buf).await?;
-
-        let msg = String::from_utf8_lossy(&buf[..n]).to_string();
-
-        if let Err(e) = tx.send(msg) {
-            eprintln!("Failed sending message {e}")
+    // iterate over FrameRead as it impl Stream
+    while let Some(Ok(msg)) = reader.next().await {
+        let message_struct: Message = serde_json::from_str(&msg)?;
+        if let Err(e) = tx.send(message_struct) {
+            eprintln!("Failed sending message {e}");
         }
     }
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
     // broadcast
-    let (tx, _) = broadcast::channel::<String>(16);
+    let (tx, _) = broadcast::channel::<Message>(64);
     loop {
         // accept and split
         let (socket, _) = listener.accept().await?;
-        let split = socket.into_split();
+        let (read_half, write_half) = socket.into_split();
+
+        //frames use \n delimeter from LinesCodec, (cant use pretty print json)
+        let reader = FramedRead::new(read_half, LinesCodec::new());
+        let writer = FramedWrite::new(write_half, LinesCodec::new());
 
         // clone sender
         let tx_clone = tx.clone();
@@ -51,9 +86,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         // read is reading chat (broadcast output), requires write socket
         // needs a receiver
-        tokio::spawn(client_read_loop(rx, split.1));
+        tokio::spawn(client_read_loop(rx, writer));
 
         // write is writing to receivers (broadcast input), requires read socket
-        tokio::spawn(client_write_loop(tx_clone, split.0));
+        tokio::spawn(client_write_loop(tx_clone, reader));
     }
 }
